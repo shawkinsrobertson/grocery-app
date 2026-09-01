b00267f37d1441a83bcf0b45d9c90e89909da79c
Seamus Robertson <s.hawkinsrobertson@gmail.com>
2026-09-01 11:22:49 -0700

    started new react app, installed bootstrap css, removed boilerplate from app.js, created header and homes components and components folder

---
+++ tdd-practice/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css'; 
+import Header from './components/Header';
+import Homes from './components/Homes';
+
+function App() {
+  return (
+   <div>
+    <Header />
+    <Homes />
+   </div>
+  );
+}
+
+export default App;
